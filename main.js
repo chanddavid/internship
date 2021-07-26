4913b44010d822da8b783d5ed7d2f7dac4128807
chanddavid <tankamansodari7@gmail.com>
2021-07-26 19:02:46 +0545

    my second commit

--- main.js
+++
@@ -1,8 +0,0 @@
-
-    const readMoreBtn = document.querySelector('.read-more-btn');
-    const text = document.querySelector('.description');
-
-    readMoreBtn.addEventListener('click',(e)=>{
-        text.classList.toggle('show-more');
-    })
- 
